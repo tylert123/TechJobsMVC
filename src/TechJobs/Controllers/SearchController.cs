2328c7a61cadfd9acf842c00e545b5707720952e
Tyler Torres <tyler@webangel.net>
2019-11-08 21:18:48 -0500

    Complete TJMVC project without Bonuses

--- src/TechJobs/Controllers/SearchController.cs
+++ src/TechJobs/Controllers/SearchController.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using Microsoft.AspNetCore.Mvc;
 using TechJobs.Models;
 
@@ -15,6 +16,29 @@ namespace TechJobs.Controllers
 
         // TODO #1 - Create a Results action method to process 
         // search request and display results
+
+        //[Route ("/Search/Index")]
+        public IActionResult Results(string searchType, string searchTerm)
+        {
+            ViewBag.columns = ListController.columnChoices;
+
+            if (searchType.Equals("all"))
+            {
+                List<Dictionary<string, string>> jobs = JobData.FindByValue(searchTerm);
+                ViewBag.title = "All " + searchTerm.First().ToString().ToUpper() + searchTerm.Substring(1) + " Jobs";
+                //ViewBag.searchType = searchType;
+                ViewBag.jobs = jobs;
+                return View("Index");
+            }
+            else
+            {
+                List<Dictionary<string, string>> jobs = JobData.FindByColumnAndValue(searchType, searchTerm);
+                ViewBag.title = searchTerm.First().ToString().ToUpper() + searchTerm.Substring(1) + " " + ListController.columnChoices[searchType] + " Jobs";
+                //ViewBag.searchType = searchType;
+                ViewBag.jobs = jobs;
+                return View("Index");
+            }
+        }
 
     }
 }
